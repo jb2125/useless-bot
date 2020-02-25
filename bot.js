887d3316965ca198ccca4ed6fb5bd084d1c451ee
jb2125 <jb2125@kent.ac.uk>
2020-02-25 13:44:42 +0000

    Tea and biscuit functionality added

--- bot.js
+++ bot.js
@@ -2,6 +2,10 @@ function answerQuery(query) {
   query = query.toLowerCase();
   if (query.indexOf("rain") != -1 || query.indexOf("sun") != -1 || query.indexOf("weather") != -1)
     return "I do not care too much about weather, I'm locked inside a data center.";
+    if (query.indexOf("tea") != -1)
+        return "I quite like tea, asam would be nice";
+    if (query.indexOf("biscuit") != -1)
+        return "Did I hear biscuit? Do you like ... have any?";
   return "Sorry Dave, I cannot do that."
 }
 
